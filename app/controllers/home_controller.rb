a38a8c81a1439ed84a9c6b03e8e818e62228c505
rinafleisch <rinafleisch@gmail.com>
2020-10-06 17:09:10 +0300

    style framework layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  # what follows is called a filter, describes where common content can
+  # be used
+  before_action :common_content, only: %i[index team]
+
+  def common_content
+    @name = 'Rina Fleisch'
+  end
+
   def index; end
 
   def team; end
