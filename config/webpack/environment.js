cce60d3b6110a26b05d2b1249249997342580214
rinafleisch <rinafleisch@gmail.com>
2020-10-06 11:58:08 +0300

    install bootstrap 4 and add nav bar

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-module.exports = environment
+// added for bootstrap
+const webpack = require('webpack');
+
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default'],
+  })
+);
+// end added
+
+module.exports = environment;
