aebaadb0919d57680c3d24d0587d427bbec965ee
rinafleisch <rinafleisch@gmail.com>
2020-10-07 10:56:16 +0300

    finish index page

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 
+# to generate fake superhero data
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,27 @@ class HomeController < ApplicationController
   before_action :common_content, only: %i[index team]
 
   def common_content
-    @name = 'Rina Fleisch'
+    @hero_name = Faker::Superhero.name
+    @hero_power = Faker::Superhero.power
+    @hero_title = Faker::Job.title
+    @hero_job = Faker::Company.bs.titleize
+    @hero_saying = Faker::TvShows::DrWho.quote
+
+    rnd_set = rand(1..2)
+    @set = if rnd_set == 1
+             'set1'
+           else
+             'set2'
+           end
+
+    bk_set = rand(1..2)
+    @bg_set = if bk_set == 1
+                'bg1'
+              else
+                'bg2'
+              end
+
+    @avatar = Faker::Avatar.image(slug: @hero_name, size: '300x300', format: 'png', set: @set, bgset: @bg_set)
   end
 
   def index; end
